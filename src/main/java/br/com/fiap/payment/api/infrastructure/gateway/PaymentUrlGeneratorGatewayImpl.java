6cd0a990a2e706d38e6768c78bdbb68be0ddd223
Felipe Santos <felipe.carvalho.bs.contato@gmail.com>
2024-12-15 02:09:39 -0300

    inclusao de actions

--- src/main/java/br/com/fiap/payment/api/infrastructure/gateway/PaymentUrlGeneratorGatewayImpl.java
+++ src/main/java/br/com/fiap/payment/api/infrastructure/gateway/PaymentUrlGeneratorGatewayImpl.java
@@ -1,16 +1,13 @@
 package br.com.fiap.payment.api.infrastructure.gateway;
 
 import br.com.fiap.payment.api.adapters.gateway.ServerUrlGeneratorGateway;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Enumeration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentUrlGeneratorGatewayImpl implements ServerUrlGeneratorGateway {
 
-  @Value("${host.ipv4.address}")
+  @Value("${host.address}")
   private String host;
   @Value("${host.port.address}")
   private int port;
@@ -20,30 +17,4 @@ public class PaymentUrlGeneratorGatewayImpl implements ServerUrlGeneratorGateway
     return host + ":" + port + "/payments/" + paymentId;
   }
 
-  private String getLocalIPv4() {
-    try {
-      Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-      while (interfaces.hasMoreElements()) {
-        NetworkInterface networkInterface = interfaces.nextElement();
-
-        // Ignora interfaces de loopback e interfaces que nao estao ativas
-        if (networkInterface.isLoopback() || !networkInterface.isUp()) {
-          continue;
-        }
-
-        Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
-        while (addresses.hasMoreElements()) {
-          InetAddress addr = addresses.nextElement();
-
-          // Verifica se o endereco e IPv4
-          if (addr.getHostAddress().indexOf(":") == -1) {
-            return addr.getHostAddress();
-          }
-        }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null; // Retorna null se nao for encontrado nenhum IPv4
-  }
 }
