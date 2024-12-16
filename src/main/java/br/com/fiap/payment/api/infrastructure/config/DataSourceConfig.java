3e28fa46ad3836514e652301fd3a223b77a4230a
Felipe Santos <felipe.carvalho.bs.contato@gmail.com>
2024-12-16 17:59:56 -0300

    Fix Datasource Issues

---
+++ src/main/java/br/com/fiap/payment/api/infrastructure/config/DataSourceConfig.java
@@ -0,0 +1,36 @@
+package br.com.fiap.payment.api.infrastructure.config;
+
+import java.util.Base64;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+  @Value("${FASTFOOD_DATABASE_USER}")
+  private String encodedUsername;
+  @Value("${FASTFOOD_DATABASE_PASSWORD}")
+  private String encodedPassword;
+  @Value("${FASTFOOD_DATABASE_URL}")
+  private String dbUrl;
+  @Value("${spring.datasource.driver-class-name}")
+  private String dbDriver;
+
+  @Bean
+  public DataSource dataSource() {
+    String decodedUsername = new String(Base64.getDecoder().decode(encodedUsername));
+    String decodedPassword = new String(Base64.getDecoder().decode(encodedPassword));
+
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setUrl(dbUrl);
+    dataSource.setDriverClassName(dbDriver);
+    dataSource.setUsername(decodedUsername);
+    dataSource.setPassword(decodedPassword);
+
+    return dataSource;
+  }
+
+}
